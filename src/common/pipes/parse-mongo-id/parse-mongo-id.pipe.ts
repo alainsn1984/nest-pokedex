9a1458a142bd56f622ebf640c3a6a6fab0e9e204
Alain Serrano <alains1984@gmail.com>
2023-01-10 23:12:41 +0100

    Primer commit Proyecto Pokedex

---
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -0,0 +1,17 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+import { BadRequestException } from '@nestjs/common/exceptions';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    //console.log({value, metadata});
+
+    if(!isValidObjectId(value)){
+      throw new BadRequestException(`${value} is not a valid MongoID `)
+    }
+
+    return value;
+  }
+}
